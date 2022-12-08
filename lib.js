f28eef9fbef8b79747ef194a0979ff25ffb62e55
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-08 14:23:03 -0400

    extract main example from lib

--- lib.js
+++ lib.js
@@ -45,23 +45,3 @@ const xzfor = (iterations = 0, ...callbacks) => {
 			callbacks[0]() //bad patch
 	}
 }
-
-/** example */
-const main = () => {
-	const log = console.log
-
-	const cbs = [
-		() => log('0'),
-		() => log('1'),
-		() => log('2')
-	]
-
-	log('z')
-	zfor(2, ...cbs)
-	//logs: 0 1 2 1 0 1 2 1 0
-
-	log('xz')
-	xzfor(2, ...cbs)
-	//logs: 0 1 2 2 1 0 0 1 2 2 1 0 (actually has an extra 0, but that's a bug)
-}
-main()
