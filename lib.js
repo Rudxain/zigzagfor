80fde434e51afd849c9dffd21fa894097e00104c
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-08 14:21:09 -0400

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,67 @@
+//@ts-check
+'use strict'
+/**
+Iterate back and forth on an array of callbacks,
+calling each callback in a "triangular waveform" order.
+
+This is a regular loop followed by a reverse loop, calling the ends once per "bounce".
+@param {number|bigint} iterations Times to iterate. Reverse iters are part of regular iters, so they aren't counted
+@param {...() => void} callbacks
+*/
+const zfor = (iterations = 0, ...callbacks) => {
+	while (iterations-- > 0) {
+		let i = 0
+		for (; i < callbacks.length; i++)
+			callbacks[i]()
+
+		i -= 2
+		for (; i > 0; i--)
+			callbacks[i]()
+
+		if (iterations < 1)
+			callbacks[0]() //bad patch
+	}
+}
+
+/**
+Iterate back and forth on an array of callbacks,
+calling each callback in a "hexagonal waveform" order.
+
+This is a regular loop followed by a reverse loop, calling the ends twice per "bounce".
+@param {number|bigint} iterations Times to iterate. Reverse iters are part of regular iters, so they aren't counted
+@param {...() => void} callbacks
+*/
+const xzfor = (iterations = 0, ...callbacks) => {
+	while (iterations-- > 0) {
+		let i = 0
+		for (; i < callbacks.length; i++)
+			callbacks[i]()
+
+		i -= 1
+		for (; i >= 0; i--)
+			callbacks[i]()
+
+		if (iterations < 1)
+			callbacks[0]() //bad patch
+	}
+}
+
+/** example */
+const main = () => {
+	const log = console.log
+
+	const cbs = [
+		() => log('0'),
+		() => log('1'),
+		() => log('2')
+	]
+
+	log('z')
+	zfor(2, ...cbs)
+	//logs: 0 1 2 1 0 1 2 1 0
+
+	log('xz')
+	xzfor(2, ...cbs)
+	//logs: 0 1 2 2 1 0 0 1 2 2 1 0 (actually has an extra 0, but that's a bug)
+}
+main()
